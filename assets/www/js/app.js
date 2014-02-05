982263bbf0fe48cf1ebd80a63668ee295f81f35d
Lucas Bickel <hairmare@purplehaze.ch>
2014-02-05 22:26:10 +0100

    initial jplayer version
    This actually works on my oldish S3 with cyanogenmod but I still haven’t
    figured out why my sparkly new htc one wont work.

--- assets/www/js/app.js
+++ assets/www/js/app.js
@@ -2,5 +2,33 @@
  * 
  */
 var startApp = function() {
-   alert('started');
+   
+   var stream = {
+       title: "RaBe",
+       mp3: "http://stream.rabe.ch:8000/livestream/rabe-low.mp3"
+   },
+   ready = false;
+
+   $("#jquery_jplayer_1").jPlayer({
+       ready: function (event) {
+           ready = true;
+           $(this).jPlayer("setMedia", stream).jPlayer("play");
+       },
+       pause: function() {
+           $(this).jPlayer("clearMedia");
+       },
+       error: function(event) {
+           if(ready && event.jPlayer.error.type === $.jPlayer.error.URL_NOT_SET) {
+               // Setup the media stream again and play it.
+               $(this).jPlayer("setMedia", stream).jPlayer("play");
+           }
+       },
+       supplied: "mp3",
+       preload: "none",
+       cssSelectorAncestor: "#jp_container",
+       swfPath: "../swf",
+       solution:"flash,html",
+       wmode: "window"
+   });
+
 };
